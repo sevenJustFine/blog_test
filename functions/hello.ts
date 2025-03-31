b0f2279b1fe4a6cb7867453b642454c922d3630f
seven li <svenjustfine@qq.com>
2025-03-31 17:59:29 +0800

    test

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,6 +1,5 @@
 export async function onRequest(context) {
-    console.error("🚀 Cloudflare Function 运行成功");
     return new Response("Hello from Cloudflare Pages Functions!", {
-        headers: { "Content-Type": "text/plain" }
+        headers: {"Content-Type": "text/plain"}
     });
 }
--- functions/logs.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    async fetch(request) {
-        console.log("🚀 Cloudflare Function 运行成功");
-        return new Response("Hello World");
-    }
-};
